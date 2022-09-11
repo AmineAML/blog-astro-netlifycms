2080a6247a35e862612e093290f33c35dd761a13
AmineAml <amine.amellouk@outlook.com>
2022-09-11 18:47:04 +0100

    chore: rename blog title

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -5,7 +5,7 @@
 <header class="navbar mb-2 shadow-lg rounded-box">
     <div class="flex-1 px-2 mx-2 justify-between">
         <div class="">
-            <p class="btn btn-ghost btn rounded-btn hover:bg-transparent"aria-label="blog"> Amine Amellouk </p>
+            <p class="btn btn-ghost btn rounded-btn hover:bg-transparent"aria-label="blog"> EchoSpaceDev </p>
         </div>
         <div class="items-stretch lg:flex">
                 <a class="btn btn-ghost btn-sm rounded-btn" href="/" aria-label="blog"> Blog </a>
