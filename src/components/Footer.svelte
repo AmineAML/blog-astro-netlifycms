14e8356bd0e03fb25475cd61bf220db3c1cd2d69
AmineAml <amine.amellouk@outlook.com>
2023-02-20 17:36:09 +0100

    chore: disable image optimization as it's depcrated and add a link to lighthouse report

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -22,6 +22,14 @@
                     <span>Sitemap</span>
                 </a>
             </li>
+            <li class="text-blue-700 underline">
+                <a href="/reports/lighthouse.html" class="flex space-x-1">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9" />
+                    </svg>
+                    <span>Lighthouse Report</span>
+                </a>
+            </li>
         </ul>
         <span class="flex justify-center">Copyright &copy; 2021, Amine Amellouk</span>
         <span class="flex flex-wrap space-x-1 justify-center">
